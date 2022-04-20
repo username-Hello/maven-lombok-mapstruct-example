52ad312620c39a40c8bea72a06b6ee607d3b65de
username-Hello <nikitos.burak@gmail.com>
2022-04-20 14:58:00 +0300

    adding lombok

---
+++ src/main/java/User.java
@@ -0,0 +1,15 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+	private String firstName;
+	private String lastName;
+	private String email;
+}
